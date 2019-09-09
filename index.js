b07de9bf49c92f18cb18f5ac78a4b05a31770d38
pramayn99 <pramay_nikhade@thbs.com>
2019-09-09 15:47:43 +0530

    /imagine added

--- index.js
+++ index.js
@@ -9,6 +9,11 @@ app.get("/dragon",(req,res)=>{
   res.send("Enter the world of dragons!") 
 })
 
+app.get("/imagine",(req,res)=>{
+  res.send("Imagine dragons!") 
+})
+
+
 app.listen(process.env.PORT||3000, ()=>{
     console.log("Application started running on Moon....")
 })
